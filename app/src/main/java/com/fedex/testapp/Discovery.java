5b5c58d1675d4618c52d26552cf5511fe60f96ae
Federico Matteoni <federicomatteoni@live.it>
2018-12-03 18:19:39 +0100

    Polling for discovery

--- app/src/main/java/com/fedex/testapp/Discovery.java
+++ app/src/main/java/com/fedex/testapp/Discovery.java
@@ -3,10 +3,16 @@ package com.fedex.testapp;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by Federico on 05/05/2015.
@@ -23,39 +29,49 @@ public class Discovery extends AsyncTask<String, String, String> {
 
     protected String doInBackground(String... params) {
         Log.d("Discovery", "avviato");
+
         try {
-            //Open a random port to send the package
             DatagramSocket c = new DatagramSocket();
             c.setBroadcast(true);
-
-            byte[] sendData = "SocketTest_discovery".getBytes();
-            try {
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 8889);
-                c.send(sendPacket);
-            } catch (Exception e) {
-            }
-
             DatagramSocket socket = new DatagramSocket(8889);
-            for (int i = 0; i < 10; i++) {
-                byte[] recvBuf = new byte[15000];
-                DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
-                socket.receive(packet);
+            do {
+                byte[] sendData = "SocketTest_discovery".getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 8889);
+                try {
+                    c.send(sendPacket);
+                } catch (IOException e) {}
 
-                publishProgress(new String(recvBuf, 0, packet.getLength()));
-                Log.d("Discovery", new String(recvBuf, 0, packet.getLength()));
-            }
-        } catch (Exception e) {
+                try {
+                    byte[] recvBuf = new byte[15000];
+                    DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+                    socket.setSoTimeout(500);
+                    socket.receive(packet);
+                    publishProgress(new String(recvBuf, 0, packet.getLength()));
+                    Log.d("Discovery", new String(recvBuf, 0, packet.getLength()));
+                } catch (SocketTimeoutException ex) {
+                } catch (IOException ex) {
+                    Log.d("Discovery", "errore pacchetto ricevuto");
+                    ex.printStackTrace();
+                }
+                c.disconnect();
+                c.close();
+                sleep(1000);
+            } while (true);
+        } catch (SocketException | UnknownHostException | InterruptedException e) {
         }
+        Log.d("Discovery", "fine");
         return "";
     }
 
     protected void onProgressUpdate(String... values) {
-        if (values[0].contains("SocketTest")) {
-            String hostname = values[0].split("&")[1].split("/")[0];
-            String ip = values[0].split("&")[1].split("/")[1];
-            servers.put(hostname, ip);
-            home.updateServers();
-            Log.d(hostname, ip);
+        if (!values[0].equals("SocketTest_discovery")) {
+            if (values[0].contains("SocketTest")) {
+                String hostname = values[0].split("&")[1].split("/")[0];
+                String ip = values[0].split("&")[1].split("/")[1];
+                servers.put(hostname, ip);
+                home.updateServers();
+                Log.d(hostname, ip);
+            }
         }
     }
 }
